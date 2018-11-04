b1e54121ad98f38b37a356bedad4100f7321056a
梁山 <liangshan@192.168.0.104>
2018-11-04 18:13:58 +0800

    Test travis ci

--- src/renderer/components/LandingPage.vue
+++ src/renderer/components/LandingPage.vue
@@ -4,7 +4,7 @@
     <main>
       <div class="left-side">
         <span class="title">
-          Welcome to Enkel!
+          Welcome to Enkel2!
         </span>
         <system-information></system-information>
       </div>
